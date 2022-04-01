da43df35bb3bb7706497c9d3dacaec983e054f79
Mehmet Ali <82174532+mehmetali9@users.noreply.github.com>
2022-04-02 00:39:13 +0300

    Add files via upload

---
+++ YildizÜcgen/Main.java
@@ -0,0 +1,38 @@
+package YildizÜcgen;
+
+import java.util.Scanner;
+
+public class Main  {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Lütfen bir sayı giriniz  : ");
+        int num = input.nextInt();
+        num /= 2;
+        num++;
+
+        for(int i = 0; i<num; i++){
+
+            for(int j = num; j> (i+1); j--)
+                System.out.print(" ");
+
+            for(int k = 0; k< (i*2)+1; k++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        int temp = 1;
+        for(int i = num-1; i> 0; i--){
+
+            for(int j = 0; j< temp; j++)
+                System.out.print(" ");
+            temp++;
+
+            for(int k=0; k< (i*2)-1; k++)
+                System.out.print("*");
+            System.out.println();
+        }
+
+    }
+}
